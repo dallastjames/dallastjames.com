2e0424c771b40400fce24fa6869c3c20706fca5d
Dallas James <dallastjames@gmail.com>
2018-08-29 21:57:24 -0600

    verify pipeline

--- apps/dallastjames/app/src/app/app.component.spec.ts
+++ apps/dallastjames/app/src/app/app.component.spec.ts
@@ -11,17 +11,17 @@ describe('AppComponent', () => {
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
-    it(`should have as title 'dallastjames'`, async(() => {
+    it(`should have as title 'dallastjames.com'`, async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('dallastjames');
+        expect(app.title).toEqual('dallastjames.com');
     }));
     it('should render title in a h1 tag', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h1').textContent).toContain(
-            'Welcome to dallastjames!'
+            'Welcome to dallastjames.com!'
         );
     }));
 });
