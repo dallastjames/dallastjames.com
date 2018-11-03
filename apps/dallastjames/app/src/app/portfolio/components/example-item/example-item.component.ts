ec108fdf63b0410fb64d64331f15fd2dfbebbc4e
Dallas James <dallastjames@gmail.com>
2018-11-03 17:17:45 -0600

    format portfolio section

--- apps/dallastjames/app/src/app/portfolio/components/example-item/example-item.component.ts
+++ apps/dallastjames/app/src/app/portfolio/components/example-item/example-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'dallastjames-example-item',
@@ -6,9 +7,26 @@ import { Component, OnInit, Input } from '@angular/core';
     styleUrls: ['./example-item.component.scss']
 })
 export class ExampleItemComponent implements OnInit {
-    @Input() public image: string = '';
+    public _image: any = '';
+    @Input()
+    public set image(value: string) {
+        if (!!value) {
+            this._image = value;
+            this.style = {
+                ...this.style,
+                'background-image': this._sanitizer.bypassSecurityTrustStyle(
+                    value
+                )
+            };
+            console.log('setting style', value, this.style);
+        }
+    }
 
-    constructor() {}
+    @Input() public dark: boolean = false;
+
+    public style: { [key: string]: any } = {};
+
+    constructor(private _sanitizer: DomSanitizer) {}
 
     ngOnInit(): void {}
 }
